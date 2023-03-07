ed288a2db7c5fd1d99045c901798e5b3dab6e86a
chris vale <christopher.vale@optum.com>
2023-03-07 10:18:57 -0800

    clean up and change model
    this started as an attempt to rebuild as a literate config. that
    proved to be more work than anticipated so it ended up being just
    condensing all config down into one file (and removing a lot of the
    old cruft)

---
+++ configuration.el
@@ -0,0 +1,271 @@
+;;; configuration.el --- emacs configuration -*- lexical-binding: t -*-
+
+;; Copyright ©  2022 Chris Vale
+;;
+;; Author: Chris Vale <crispywalrus@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; load various Emacs extensions that make my life easier and more productive.
+
+;;; Code:
+
+;; configure our GUI appearance. no scrollbar or toolbars and set the
+;; font to Hack 12.
+(when (display-graphic-p)
+  (setq initial-frame-alist nil
+        default-frame-alist nil)
+  (set-frame-font "Hack-12" nil t)
+  (scroll-bar-mode -1)
+  (tool-bar-mode -1)
+  (windmove-default-keybindings))
+
+;; setup key bindings to allow for both super and hyper to have useful
+;; bindings from darwin running device. also paper over the
+;; differences between the way keys are named in emace-plus and
+;; railwaycats distributions.
+(when (eq system-type 'darwin)
+  ;; mac osx, use ns-* settings to distiguish between the flavors of
+  ;; emacs available. if we're on darwin use ns-use-native-fullscreen
+  ;; to determine if we're using emacs-plus or railwaycats
+  (if (boundp 'ns-use-native-fullscreen)
+      ;; emacs-plus
+      (progn
+        (setq ns-use-native-fullscreen t
+              ns-command-modifier 'meta
+              ns-option-modifier 'super
+              ns-right-option-modifier 'hyper)
+        (global-set-key (kbd "M-h") 'ns-do-hide-emacs))
+      ;; else railwaycats
+      (setq mac-command-modifier 'meta
+            mac-option-modifier 'super
+            ;;            mac-right-option-modifier 'hyper))
+            mac-right-option-modifier 'hyper)
+    ()))
+
+;; make use-package download all referenced but uninstalled
+;; packages.
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+(use-package exec-path-from-shell
+  :init (exec-path-from-shell-initialize))
+
+(setenv "JAVA_HOME" "/Users/cvale2/.sdkman/candidates/java/current")
+(setenv "PATH" (concat "/Users/cvale2/.sdkman/candidates/java/current/bin:" (getenv "PATH")))
+
+;; for code we can't just use from a package manager we'll check it
+;; into the vendor tree and manage it by hand.
+(add-to-list 'load-path (expand-file-name "vendor" user-emacs-directory))
+
+;; these are various elisp coding and data structure
+;; libraries. they're not user modes they're elisp
+;; enhancements. Sometimes the modes and extensions used rely on them,
+;; but I also use them for local elisp development. Since these are
+;; just coding tools they tend to not need any configuration.
+(use-package s)
+(use-package string-inflection
+  :bind ("s-i" . string-inflection-all-cycle))
+(use-package dash)
+(use-package m-buffer)
+(use-package f)
+;; (use-package multiple-cursors
+;;   :ensure t)
+(use-package suggest)
+(use-package parsec)
+(use-package pfuture)
+(use-package async)
+(use-package memoize)
+
+;; packages
+;; my default customization
+
+;; I use a .gitignored custom.el file so I can maintain different
+;; configs per system. Loading fails if the file doesn't exist so we
+;; touch it to make sure emacs always starts.
+(f-touch (expand-file-name "custom.el" user-emacs-directory))
+
+;; no tabs
+(setq-default indent-tabs-mode nil)
+
+;; I feel a bit curmudgeonly about this but no to menus, no to
+;; scrollbars, no to toolbars, no to the scratch buffer message, no to
+;; the startup screen.
+(setq
+ inhibit-startup-screen t
+ initial-scratch-message nil
+ custom-file (expand-file-name "custom.el" user-emacs-directory)
+ load-prefer-newer t
+ debug-on-error nil)
+
+;; up abover we touched custom.el. we did this so that there was
+;; definately going to be a file. now we can load it in relative
+;; safety.
+(load custom-file)
+
+;; don't ask about narrow-to-regeion
+(put 'narrow-to-region 'disabled nil)
+
+(add-hook 'dired-load-hook (lambda () (require 'dired-x)))
+
+;; for some reason the mac version of emacs has decided to use / as
+;; the default directory. That's not great for usability.
+(setq default-directory "~/")
+
+(use-package nano-theme)
+
+(use-package all-the-icons
+  :if (display-graphic-p))
+
+(use-package all-the-icons-dired
+  :after all-the-icons
+  :hook (dired-mode . all-the-icons-dired-mode)
+  :config (setq all-the-icons-dired-monochrome nil))
+
+;; completion
+(use-package hydra)
+
+(use-package ivy
+  :config
+  (ivy-mode)
+  (setq ivy-use-selectable-prompt t
+        ivy-use-virtual-buffers t))
+
+;; (use-package counsel)
+
+(use-package ivy-posframe
+  :after posframe
+  :config
+  (ivy-posframe-mode 1)
+  (setq ivy-posframe-display-functions-alist
+      '((swiper          . ivy-display-function-fallback)
+        (complete-symbol . ivy-posframe-display-at-point)
+        (counsel-M-x     . ivy-posframe-display-at-window-bottom-left)
+        (t               . ivy-posframe-display))))
+
+;; general programing IDE
+(use-package lsp-mode
+  :config (setq lsp-enable-snippet nil)
+  (setq gc-cons-threshold 100000000)
+  (setq read-process-output-max (* 1024 1024))
+  :hook (merlin-mode . lsp)
+        (lsp-mode . lsp-lens-mode))
+
+(use-package lsp-ui)
+
+(use-package lsp-ivy)
+
+;; for some reason this still needs to be added by hand
+(use-package lsp-metals
+  :ensure t
+  :custom
+  ;; Metals claims to support range formatting by default but it supports range
+  ;; formatting of multiline strings only. You might want to disable it so that
+  ;; emacs can use indentation provided by scala-mode.
+  (lsp-metals-server-args '("-J-Dmetals.allow-multiline-string-formatting=off"))
+  :hook (scala-mode . lsp))
+
+;; Posframe is a pop-up tool that must be manually installed for dap-mode
+(use-package posframe)
+
+(use-package dap-mode
+  :hook (lsp-mode . dap-mode)
+        (lsp-mode . dap-ui-mode))
+
+(use-package sbt-mode
+  :init (setq sbt:prefer-nested-projects t)
+  :commands sbt-start sbt-command sbt-hydra
+  :config (substitute-key-definition
+           'minibuffer-complete-word
+           'self-insert-command
+           minibuffer-local-completion-map))
+
+(use-package company)
+
+(use-package scala-mode
+  :config
+  (require 'scala-mode-prettify-symbols)
+  (setq prettify-symbols-alist scala-prettify-symbols-alist)
+  :init
+  (add-hook 'scala-mode-hook 'company-mode)
+  :bind
+  ("C-c C-b" . sbt-hydra)
+  :interpreter
+  ("scala" . scala-mode))
+
+;; project management
+(use-package projectile
+  :init
+  (setq projectile-enable-caching t)
+  :config
+  (setq projectile-completion-system 'ido)
+  (projectile-mode +1)
+
+  :bind-keymap (("s-p" . projectile-command-map)
+                ("C-c p" . projectile-command-map)))
+
+(use-package counsel-projectile
+  :after projectile
+  :config
+  (counsel-projectile-mode))
+
+(use-package smithy-mode)
+
+;; mermaid is a package for laying out graphs in markdown and other
+;; documents. It's rendered in github docs so that makes it a useful
+;; package.
+(use-package mermaid-mode)
+
+(use-package markdown-mode)
+
+(use-package yaml-mode)
+
+(use-package org
+  :ensure t
+  :config
+  (setq org-directory (expand-file-name "~/.org")
+        org-default-notes-file (concat org-directory "/notes.org"))
+  :bind (("C-c l" . org-store-link)
+         ("C-c c" . org-capture)
+         ("C-c a" . org-agenda)
+         ("C-c b" . org-iswitchb)
+         ("C-c C-w" . org-refile)
+         ("C-c j" . org-clock-goto)
+         ("C-c C-x C-i" . org-clock-in)
+         ("C-c C-x C-o" . org-clock-out)))
+
+(use-package org-fancy-priorities
+  :init
+  (setq org-priority-highest 0
+        org-priority-default 2
+        org-priority-lowest 4
+        org-fancy-priorities-list '(
+                                    (?0 . "P0")
+                                    (?1 . "P1")
+                                    (?2 . "P2")
+                                    (?3 . "P3")
+                                    (?4 . "P4"))
+        org-priority-faces '((?0 :foreground "DarkRed" :background "LightPink")
+                             (?1 :foreground "DarkOrange4" :background "LightGoldenrod")
+                             (?2 :foreground "gray20" :background "gray")
+                             (?3 :foreground "gray20" :background "gray")
+                             (?4 :foreground "gray20" :background "gray"))))
+
+(use-package org-kanban)
+
+;;; configuration.el ends here
--- init.el
+++ init.el
@@ -22,24 +22,14 @@
 
 ;;; Code:
 
-;; try to tune gc to get fast reliable startups
-(setq gc-cons-threshold 100000000)
-(add-hook 'after-init-hook (lambda ()
-                             (setq gc-cons-threshold 800000)))
-
-(setq read-process-output-max (* 1024 1024))
-
-
-
-;; preable, require up the emacs built in package manager.
+;; preable, load package manager (builtin)
 (require 'package)
 
-;; configure package to use melpa, org, and melpa-stable
+;; configure package package manager to use melpa and org repos
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
                          
-
-;; and finaglly initialize package manager
+;; initialize package manager
 (package-initialize)
 
 ;; the rest of this uses use-package to manage loading and configuring
@@ -51,112 +41,18 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(use-package benchmark-init
+;; magit is so important we load it here rather than in
+;; configuration.el
+(use-package magit
   :ensure t
-  :config
-  ;; To disable collection of benchmark data after init is done.
-  (add-hook 'after-init-hook 'benchmark-init/deactivate))
+  :commands magit-status magit-blame
+  :init
+  (setq magit-auto-revert-mode nil
+        magit-last-seen-setup-instructions "1.4.0")
+  :bind (("s-g" . magit-status)
+         ("s-b" . magit-blame)))
 
-;; make use-package download all referenced but uninstalled
-;; packages.
-(require 'use-package-ensure)
-(setq use-package-always-ensure t)
+;; load our real configuration
+(load ( expand-file-name "~/.emacs.d/configuration.el"))
 
-(use-package exec-path-from-shell
-  :init (exec-path-from-shell-initialize))
-(setenv "PATH" (concat "/Users/cvale2/.sdkman/candidates/java/current/bin:" (getenv "PATH")))
-
-;; I've broken out the more complex setup of my dev environment into
-;; local buffs. each buff respresents a particular area of emacs
-;; configured the way I like it.
-(add-to-list 'load-path (expand-file-name "buffs" user-emacs-directory))
-(add-to-list 'load-path (expand-file-name "appearance" user-emacs-directory))
-(add-to-list 'load-path (expand-file-name "staging" user-emacs-directory))
-(add-to-list 'load-path (expand-file-name "vendor" user-emacs-directory))
-
-;; these are various elisp coding and data structure
-;; libraries. they're not user modes they're elisp
-;; enhancements. Sometimes the modes and extensions used rely on them,
-;; but I also use them for local elisp development.
-(use-package s)
-(use-package string-inflection
-  :bind ("s-i" . string-inflection-all-cycle))
-(use-package dash)
-(use-package m-buffer)
-(use-package f)
-(use-package multiple-cursors)
-(use-package suggest)
-(use-package parsec)
-(use-package pfuture)
-(use-package async)
-
-(f-mkdir (expand-file-name "staging" user-emacs-directory))
-
-;; packages
-;; my default customization
-
-;; I use a .gitignored custom.el file so I can maintain different
-;; configs per system. Loading fails if the file doesn't exist so we
-;; touch it to make sure emacs always starts.
-(f-touch (expand-file-name "custom.el" user-emacs-directory))
-
-;; no tabs
-(setq-default indent-tabs-mode nil)
-
-;; I feel a bit curmudgeonly about this but no to menus, no to
-;; scrollbars, no to toolbars, no to the scratch buffer message, no to
-;; the startup screen.
-(setq
- inhibit-startup-screen t
- initial-scratch-message nil
- custom-file (expand-file-name "custom.el" user-emacs-directory)
- load-prefer-newer t
- debug-on-error nil)
-
-;; up abover we touched custom.el. we did this so that there was
-;; definately going to be a file. now we can load it in relative
-;; safety.
-(load custom-file)
-
-;; don't ask about narrow-to-regeion
-(put 'narrow-to-region 'disabled nil)
-
-;; configure our GUI appearance. no scrollbar or toolbars and set the
-;; font to Hack 12.
-(when (display-graphic-p)
-  (setq initial-frame-alist nil
-        default-frame-alist nil)
-  (set-frame-font "Hack-12" nil t)
-  (scroll-bar-mode -1)
-  (tool-bar-mode -1)
-  (windmove-default-keybindings))
-;; end my defaults
-
-;; setup key bindings to allow for both super and hyper to have useful
-;; bindings. also paper over the differences between the defaults in
-;; the stock and railway cats distributions.
-(when (eq system-type 'darwin)
-  ;; mac osx, use ns-* settings to distiguish between the flavors of emacs available.
-  (if (boundp 'ns-use-native-fullscreen)
-      (progn
-        (setq ns-use-native-fullscreen t
-              ns-command-modifier 'meta
-              ns-option-modifier 'super
-              ns-right-option-modifier 'hyper)
-        (global-set-key (kbd "M-h") 'ns-do-hide-emacs))
-    (progn
-      (setq mac-command-modifier 'meta
-            mac-option-modifier 'super
-            mac-right-option-modifier 'hyper))
-    ()))
-
-(add-hook 'dired-load-hook (lambda () (require 'dired-x)))
-
-;; for some reason the mac version of emacs has decided to use / as
-;; the default directory. That's not great for usability.
-(setq default-directory "~/")
-
-; (require 'mkpretty)
-
-(require 'buffs)
 ;;; init.el ends here
